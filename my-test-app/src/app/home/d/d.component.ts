ee3575e9931b84ac8201f807186e24eb2624dfc9
DovhaNataliia <tuskia224@gmail.com>
2017-11-07 21:25:12 +0200

    angular(1)

---
+++ my-test-app/src/app/home/d/d.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+@Component({
+  selector: 'app-d',
+  templateUrl: './d.component.html',
+  styleUrls: ['./d.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
